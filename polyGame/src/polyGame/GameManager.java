0fdf1e4dc6faf194ad0c5342bc263d1a151b26c5
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-04-01 17:59:57 +0900

    add : run() 메소드 추가

--- polyGame/src/polyGame/GameManager.java
+++ polyGame/src/polyGame/GameManager.java
@@ -1,5 +1,21 @@
 package polyGame;
 
 public class GameManager {
-
+	
+	private PrintText text = new PrintText();
+	
+	private boolean isRun() {
+		return true;
+	}
+	
+	private void game() {
+		while(isRun()) {
+			
+		}
+	}
+	
+	public void run() {
+		text.intro();
+		
+	}
 }
