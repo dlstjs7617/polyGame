e4a59ef403e2df59665c94d358271bbe7d8d496d
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-04-02 10:46:40 +0900

    add : 맵세팅 메소드 추가

--- polyGame/src/polyGame/MapManager.java
+++ polyGame/src/polyGame/MapManager.java
@@ -1,5 +1,51 @@
 package polyGame;
 
-public class MapManager {
+import java.util.ArrayList;
+import java.util.Random;
 
+public class MapManager {
+	
+	private ArrayList<ArrayList<DungeonMap>> map = new ArrayList<>();
+	private Random ran = new Random();
+	
+	private final int SIZE = 10;
+	
+	private int y = 0;
+	private int x = 0;
+	
+	public MapManager() {
+		
+	}
+	
+	public void printMap() {
+		for(int i=0; i<SIZE; i++) {
+			for(int j=0; j<SIZE; j++) {
+				System.out.print(map.get(i).get(j));
+			}
+			System.out.println();
+		}
+	}
+	
+	
+	private void setGoal() {
+		int rY = ran.nextInt(SIZE);
+		int rX = ran.nextInt(SIZE);
+		map.get(rY).get(rX).setGoal(false);
+	}
+	
+	private void setMap() {
+		
+		for(int i=0; i<SIZE; i++) {
+			ArrayList<DungeonMap> temp = new ArrayList<DungeonMap>();
+			for(int j=0; j<SIZE; j++) {
+				int rNum = ran.nextInt(5)+1;
+				DungeonMap dunMap = new DungeonMap(rNum);
+				temp.add(dunMap);
+			}
+			map.add(temp);
+		}
+	}
+	
+	
+	
 }
