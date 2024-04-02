94b4541deb8312024b2e4b8fe5a5f9ef76eec2e8
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-04-02 10:44:47 +0900

    add : 맵에대한 필드값 추가 및 게터세터 추가 toString 오버라이드

--- polyGame/src/polyGame/DungeonMap.java
+++ polyGame/src/polyGame/DungeonMap.java
@@ -1,5 +1,38 @@
 package polyGame;
 
 public class DungeonMap {
+	private int encount;
+	private boolean check;
+	private boolean goal;
+	private boolean player;
 	
+	public DungeonMap(int num) {
+		this.encount = num;
+	}
+	
+	public int getEncount() {
+		return this.encount;
+	}
+	
+	public boolean getCheck() {
+		return this.check;
+	}
+	
+	public void setCheck(boolean check) {
+		this.check = check;
+	}
+	
+	public boolean getGoal() {
+		return this.goal;
+	}
+	
+	public void setGoal(boolean goal) {
+		this.goal = goal;
+	}
+	
+	
+	@Override
+	public String toString() {
+		return String.format("%s ", player == true ? "▶" : check == true ? "■" : "□");
+	}
 }
