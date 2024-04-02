2a85c580a1a378b129e1c519a8cb297ea1ef3718
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-04-02 14:26:09 +0900

    modfiy: 출력문 수정

--- polyGame/src/polyGame/StageMove.java
+++ polyGame/src/polyGame/StageMove.java
@@ -2,9 +2,19 @@ package polyGame;
 
 public class StageMove extends Stage{
 
+	private MapManager map = MapManager.getInstance();
+	
 	@Override
 	public boolean update() {
-		return false;
+		boolean run = true;
+		while(true) {			
+			map.printMap();
+			if(map.move()) {
+				return false;
+			}else {
+				continue;
+			}
+		}
 	}
 
 	@Override
