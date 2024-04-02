b360be23c94c09ce2dcba165f9df7f9c4983294c
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-04-02 16:26:12 +0900

    modify: 출력문 변경

--- polyGame/src/polyGame/DungeonMap.java
+++ polyGame/src/polyGame/DungeonMap.java
@@ -33,6 +33,6 @@ public class DungeonMap {
 	
 	@Override
 	public String toString() {
-		return String.format("", check == true ? "■" : "□");
+		return String.format("%s", check == true ? "■ " : "□ ");
 	}
 }
