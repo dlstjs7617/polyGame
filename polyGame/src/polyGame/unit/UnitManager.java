1e5990063b6833cc9c947f52e28c5d628866e6ab
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-04-04 14:24:22 +0900

    modify 패키지추가및 위치변경으로 path값 수정

--- polyGame/src/polyGame/unit/UnitManager.java
+++ polyGame/src/polyGame/unit/UnitManager.java
@@ -1,9 +1,8 @@
-package polyGame.unit.monster;
+package polyGame.unit;
 
 import java.util.ArrayList;
 import java.util.Random;
 
-import polyGame.unit.Unit;
 import polyGame.unit.player.Healer;
 import polyGame.unit.player.Warrior;
 import polyGame.unit.player.Wizard;
@@ -13,8 +12,8 @@ public class UnitManager {
 	public static ArrayList<Unit> players = new ArrayList<Unit>();
 	public static ArrayList<Unit> monster = new ArrayList<Unit>();
 	
-	private String path = "polyGame.";
-	private String[] mons = { "UnitBat", "UnitOrc", "UnitSlime", "UnitWolf" };
+	private String path = "polyGame.unit.monster.";
+	private String[] mons = { "Bat", "Orc", "Slime", "Wolf" };
 	private Random ran = new Random();
 	
 	public UnitManager() {
@@ -28,12 +27,11 @@ public class UnitManager {
 			int rNum = ran.nextInt(mons.length);
 			
 			try {
+				int ranLevel = ran.nextInt(5)+avgLevel();
 				Class<?> clazz = Class.forName(path + mons[rNum]);
-				Object obj = clazz.getDeclaredConstructor().newInstance();
+				Object obj = clazz.getDeclaredConstructor().newInstance(ranLevel);
 				Unit unit = (Unit)obj;
 				
-				int ranLevel = ran.nextInt(5)+avgLevel();
-				unit = monsterStatus(unit, ranLevel);
 				monster.add(unit);
 			} catch (Exception e) {
 				System.err.println("찾지 못한 클래스명");
@@ -42,46 +40,10 @@ public class UnitManager {
 		}
 	}
 	
-	private Unit monsterStatus(Unit unit, int level) {
-		Unit result = unit;
-		if(unit.getName().equals("박쥐")) {
-			int hp = ran.nextInt(level * 5) + 100;
-			int mp = ran.nextInt(level * 3) + 50;
-			int power = ran.nextInt(level * 2) +10;
-			int luck = level/3;
-			int defense = level/5;
-			int dex = level/2;
-		}else if(unit.getName().equals("오크")) {
-			int hp = ran.nextInt(level * 10) + 200;
-			int mp = ran.nextInt(level * 5) + 70;
-			int power = ran.nextInt(level * 3) +15;
-			int luck = level/5;
-			int defense = level/2;
-			int dex = level/10;
-			
-		}else if(unit.getName().equals("슬라임")) {
-			int hp = ran.nextInt(level * 5) + 100;
-			int mp = ran.nextInt(level * 5) + 50;
-			int power = ran.nextInt(level) +10;
-			int luck = level/10;
-			int defense = level/10;
-			int dex = level/10;
-			
-		}else if(unit.getName().equals("늑대")) {
-			int hp = ran.nextInt(level * 10) + 100;
-			int mp = ran.nextInt(level * 10) + 50;
-			int power = ran.nextInt(level * 3) +5;
-			int luck =  level/3;
-			int defense = level/7;
-			int dex = level/10;
-			
-		}
-		
-		return result;
-	}
+	
 	
 	private int avgLevel() {
-		int level = 0;
+		int level = 1;
 		for(int i=0; i<players.size(); i++) {
 			level += players.get(i).getLevel();
 		}
