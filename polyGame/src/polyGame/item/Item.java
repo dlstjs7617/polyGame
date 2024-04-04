e7c61f3fa7c573fa76138b4b73ee8b55c5900d6d
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-04-04 17:33:45 +0900

    add : 수량 메소드 추가

--- polyGame/src/polyGame/item/Item.java
+++ polyGame/src/polyGame/item/Item.java
@@ -4,17 +4,20 @@ public class Item {
 	static final int WEAPON = 1; // 무기
 	static final int ARMOR = 2; // 갑옷
 	static final int RING = 3; // 장신구
+	static final int POTION = 4; // 포션
+	
 	int kind; // 종류
 	String name; // 이름
-	int power; // 능력
+	int status; // 능력
 	int price; // 가격
-
+	int ea;	   // 수량
+	
 	// 아이템 설정 메서드
-	public void setItem(int k, String n, int p, int pr) {
-		kind = k;
-		name = n;
-		power = p;
-		price = pr;
+	public void setItem(int kind, String name, int status, int price) {
+		this.kind = kind;
+		this.name = name;
+		this.status = status;
+		this.price = price;
 	}
 
 }
