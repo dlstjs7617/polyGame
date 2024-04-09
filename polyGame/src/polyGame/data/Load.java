75d92e36ccd6889f30702788dac328a97d17ccf1
dlstj <dlstj@DESKTOP-DPCQNGK>
2024-04-09 12:40:40 +0900

    add : load에 필요한 필드값 추가

--- polyGame/src/polyGame/data/Load.java
+++ polyGame/src/polyGame/data/Load.java
@@ -1,5 +1,15 @@
 package polyGame.data;
 
-public class Load {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 
+public class Load {
+	private String fileName = "save.txt";
+	private File file = new File(fileName);
+	private FileReader fr;
+	private BufferedReader br;
+	
+	
+	
 }
